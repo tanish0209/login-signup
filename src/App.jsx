ebb8e28351c69a48626fd8704caca6a20f3f1ec8
tanish0209 <112083355+tanish0209@users.noreply.github.com>
2024-07-31 11:06:37 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { LoginSign } from "./modules/components/LoginSign";
+
+export const App = () => {
+  return <LoginSign />;
+};
+export default App;
---
+++ src/modules/components/LoginSign.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loginsign.css";
+
+export const LoginSign = () => {
+  return (
+    <div className="log">
+      <div className="sub">Login</div>
+
+      <div className="sub">Sign Up</div>
+    </div>
+  );
+};
